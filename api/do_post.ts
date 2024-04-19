43d3905de3bc789eb2f802a83c5367ab105bf29d
Sudono Tanjung <sudtanj.business@gmail.com>
2024-04-19 10:45:12 +0700

    fix responder helper error

--- api/do_post.ts
+++ api/do_post.ts
@@ -1,23 +1,23 @@
 import {GetEvent} from "../interface/get_event";
 import {PostBody, PostBodyPath} from "../interface/post_body";
-import {sendNotFound, sendSuccess, sendUnauthorized} from "../lib/responder_helper";
 import {GoogleCalendarHelper} from "../lib/google_calendar_helper";
+import {ResponderHelper} from "../lib/responder_helper";
 
 function doPost(e: GetEvent) {
     const body: PostBody = JSON.parse(e.postData.contents) as PostBody
     const secretToken = PropertiesService.getScriptProperties().getProperty("api_secret_token")
     if (body.secretToken != secretToken) {
-        return sendUnauthorized("invalid token " + body.secretToken)
+        return ResponderHelper.sendUnauthorized("invalid token " + body.secretToken)
     }
     switch (body.path) {
         case PostBodyPath.IS_TODAY_OUT_OF_OFFICE:
             return isTodayOutOfOfficeHandler()
         default:
-            return sendNotFound("invalid path!")
+            return ResponderHelper.sendNotFound("invalid path!")
     }
 }
 
 function isTodayOutOfOfficeHandler() {
     const res = GoogleCalendarHelper.isTodayOutOfOffice()
-    return res ? sendSuccess(res) : sendNotFound(res)
+    return res ?  ResponderHelper.sendSuccess(res) :  ResponderHelper.sendNotFound(res)
 }
--- lib/responder_helper.ts
+++ lib/responder_helper.ts
@@ -1,28 +1,30 @@
 import {ApiResponse} from "../interface/api_response";
 
-export function sendSuccess(data: any) {
-    const respond: ApiResponse = {
-        status: 200,
-        message: "success",
-        data: data
+export class ResponderHelper {
+    static sendSuccess(data: any) {
+        const respond: ApiResponse = {
+            status: 200,
+            message: "success",
+            data: data
+        }
+        return ContentService.createTextOutput(JSON.stringify(respond)).setMimeType(ContentService.MimeType.JSON);
     }
-    return ContentService.createTextOutput(JSON.stringify(respond)).setMimeType(ContentService.MimeType.JSON);
-}
 
-export function sendNotFound(data: any) {
-    const respond: ApiResponse = {
-        status: 404,
-        message: "not found",
-        data: data
+    static sendNotFound(data: any) {
+        const respond: ApiResponse = {
+            status: 404,
+            message: "not found",
+            data: data
+        }
+        return ContentService.createTextOutput(JSON.stringify(respond)).setMimeType(ContentService.MimeType.JSON);
     }
-    return ContentService.createTextOutput(JSON.stringify(respond)).setMimeType(ContentService.MimeType.JSON);
-}
 
-export function sendUnauthorized(data: any) {
-    const respond: ApiResponse = {
-        status: 401,
-        message: "unauthorized",
-        data: data
+    static sendUnauthorized(data: any) {
+        const respond: ApiResponse = {
+            status: 401,
+            message: "unauthorized",
+            data: data
+        }
+        return ContentService.createTextOutput(JSON.stringify(respond)).setMimeType(ContentService.MimeType.JSON);
     }
-    return ContentService.createTextOutput(JSON.stringify(respond)).setMimeType(ContentService.MimeType.JSON);
 }
