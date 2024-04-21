0cc6e80b769c3897a06488fe0cb3a7441c11e1d1
Sudono Tanjung <sudtanj.business@gmail.com>
2024-04-21 10:44:37 +0700

    fix url bulder

--- lib/google_maps_client.ts
+++ lib/google_maps_client.ts
@@ -8,15 +8,13 @@ export class GoogleMapsClient {
 	}
 
 	getTimeEstimate(origin: string, dest: string, departTime: string) {
-		const params = new URLSearchParams({
-			origin: origin,
-			destination: dest,
-			key: this.key,
-			traffic_model: "best_guess",
-			departure_time: departTime,
-		})
-		const url = "https://maps.googleapis.com/maps/api/directions/json?" + params.toString()
-		const resData = UrlFetchAppHelper.fetchAsObject<any>(url, {})
+		const urlDat  = new URL("https://maps.googleapis.com/maps/api/directions/json")
+		urlDat.searchParams.set("origin", origin)
+		urlDat.searchParams.set("destination", dest)
+		urlDat.searchParams.set("key", this.key)
+		urlDat.searchParams.set("traffic_model", "best_guess")
+		urlDat.searchParams.set("departure_time", departTime)
+		const resData = UrlFetchAppHelper.fetchAsObject<any>(urlDat.toString(), {})
 		const duration = resData?.routes?.[0]?.legs?.[0]?.duration_in_traffic
 
 		return duration as { text: string, value: number }
