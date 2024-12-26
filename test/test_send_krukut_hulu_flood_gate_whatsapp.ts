9e09869c2e12cd4dedaf7099c3619d89a72518a0
Sudono Tanjung <17475015+sudtanj@users.noreply.github.com>
2024-12-26 17:19:30 +0700

    update test

--- test/test_send_krukut_hulu_flood_gate_whatsapp.ts
+++ test/test_send_krukut_hulu_flood_gate_whatsapp.ts
@@ -7,11 +7,11 @@ async function TestSendKrukutHuluFloodGateWhatsapp() {
     if (!floodGateDataById) {
         return null
     }
-    text = `Krukut Hulu flood gate is ${floodGateDataById.TINGGI_AIR}, previous height is ${floodGateDataById.TINGGI_AIR_SEBELUMNYA} with status ${floodGateDataById.STATUS_SIAGA} \n`
+    text = `Krukut Hulu flood gate is ${floodGateDataById.TINGGI_AIR}, previous height is ${floodGateDataById.TINGGI_AIR_SEBELUMNYA} with status ${floodGateDataById.STATUS_SIAGA}, `
+    await CallMeBotWhatsAppAPI.sendMessage(text)
     const weatherForecasts = FloodGateDataService.fetchWeatherForecast(false)
     for (const weatherForecast of weatherForecasts) {
-        text = text + `Weather forecast for ${weatherForecast.daerah} in morning is  ${weatherForecast.pagi}, in day is ${weatherForecast.siang}, in night is ${weatherForecast.malam} \n`
+        text = `Weather forecast for ${weatherForecast.daerah} in morning is  ${weatherForecast.pagi}, in day is ${weatherForecast.siang}, in night is ${weatherForecast.malam},`
+        await CallMeBotWhatsAppAPI.sendMessage(text)
     }
-
-    await CallMeBotWhatsAppAPI.sendMessage(text)
 }
